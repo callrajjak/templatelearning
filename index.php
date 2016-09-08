8ed8553fb5eab5e7ef3eed016fbf64dd2d452747
callrajjak <callrajjak@gmail.com>
2016-09-08 00:54:50 -0700

    files commited

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>my first site</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="wrapper">
+<div id="header" class="borderSite">
+	<h1>	this is header part
+	</h1>
+</div>
+<div id="menu" class="borderSite">
+	<h3>this is menu</h3>
+</div>
+<div id="leftcol" class="borderSite">
+	<h3>this is left column</h3>
+</div>
+<div id="center" class="borderSite">
+	<h3>this is center part</h3>
+</div>
+<div id="rightcol" class="borderSite">
+	<h3>this is right column</h3>
+</div>
+<div id="footer" class="borderSite">
+	<h3>this is footer</h3>
+</div>
+</div>
+</body>
+</html>
